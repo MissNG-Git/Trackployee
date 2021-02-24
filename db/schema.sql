a6a8fe99f51273588baabdc0eebb2fd62da94568
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-02-25 00:33:37 +1100

    create schema & seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,30 @@
 -- DROP DB
+DROP DATABASE IF EXISTS trackployee_db;
 -- CREATE DB
-
+CREATE DATABASE trackployee_db;
 -- USE DB
+USE trackployee_db;
 
 -- CREATE TABLES: department, role & employee
--- **REF: Readme Instructions**
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL, 
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT DEFAULT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,45 @@
 -- USE DB
+USE trackployee_db;
 
 -- Seed data into department, role & employee...
--- INSERT INTO tableName (colName) VALUES (colName.vals)
+INSERT INTO department 
+    (name)
+VALUE 
+    ("Executive"),
+    ("Finance"),
+    ("Human Resources"),
+    ("Operations"),
+    ("Sales");
+
+INSERT INTO role 
+    (title, salary, department_id)
+VALUE 
+    -- Finance
+    ("Accountant", 58468, 2),
+    -- Exec
+    ("CEO", 154863, 1),
+    -- Ops
+    ("Customer Service Rep", 51226, 4),
+    ("Office Manager", 61174, 4), -- M.ID 1
+    ("Receptionist", 44536, 4),
+    -- HR
+    ("Recruiter", 61538, 3),
+    -- Sales
+    ("Sales Manager", 70000, 5), -- M.ID 2
+    ("Salesperson", 54709, 5);
+
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id)
+VALUE 
+    ("Bob", "Beau", 4, null), -- Off Mgr
+    ("Jane", "Doe", 8, 2),
+    ("Jim", "Bob", 5, 1),
+    ("Joe", "Schmoe", 2, null), --CEO
+    ("John", "Smith", 1, 1),
+    ("Mary", "Jane", 3, 1),
+    ("Tom", "Mott", 6, 1),
+    ("Yu", "Mi", 7, null); -- Sales Mgr
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
